17f0d1cbe34599b1b3e6f71649c84d0eb9de6b8f
fiandev <wongkeras2@gmail.com>
2023-03-10 20:53:11 +0700

    Add: emptyDirSync method for generete Folders

--- bin/installer.js
+++ bin/installer.js
@@ -1,14 +1,23 @@
 #!/usr/bin/env node
 
 const { resolve, join } = require("path");
-const { copySync, pathExists } = require("fs-extra");
+const { copySync, pathExists, emptyDirSync } = require("fs-extra");
 
-const dest = resolve(process.cwd());
+
+const createSlug = (text) => {
+  return text.replace(/(\`|\'|\")/, "").split(" ").join("-").split("_").join("-");
+};
+
+const project_name = createSlug(process.argv[2]);
+const path_project = join(process.cwd(), project_name);
+
+const dest = resolve(path_project);
 const template = join(__dirname, "../template");
 const env_example = join(process.cwd(), ".env.example");
 const path_env = join(process.cwd(), ".env");
 
 const main = () => {
+  emptyDirSync(path_project);
   copySync(template, dest);
   if (pathExists(env_example)) moveSync(env_example, path_env);
 };
