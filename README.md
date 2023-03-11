0b093a8733c2c39661b499ed6bfccf0f0adf75df
fiandev <wongkeras2@gmail.com>
2023-03-11 14:18:26 +0700

    Release: express-structure-folders@2.0.0

---
+++ README.md
@@ -0,0 +1,8 @@
+# express structure folders
+
+## how to usage
+
+### create express app
+```shell
+npx express-app myapp
+```
--- bin/installer.js
+++ bin/installer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const { resolve, join } = require("path");
-const { copySync, pathExists, emptyDirSync } = require("fs-extra");
-
+const { resolve, join, basename, parse } = require("path");
+const { copySync, existsSync, emptyDirSync, moveSync } = require("fs-extra");
+const { readdirSync } = require("fs");
 
 const createSlug = (text = "") => {
   return text.toString().replace(/(\`|\'|\")/, "").split(" ").join("-").split("_").join("-");
@@ -11,6 +11,7 @@ const createSlug = (text = "") => {
 const project_name = createSlug(process.argv[2]);
 const path_project = join(process.cwd(), project_name);
 
+const static_files = join(__dirname, "../static");
 const dest = resolve(path_project);
 const template = join(__dirname, "../template");
 const env_example = join(process.cwd(), ".env.example");
@@ -19,7 +20,12 @@ const path_env = join(process.cwd(), ".env");
 const main = () => {
   emptyDirSync(path_project);
   copySync(template, dest);
-  if (pathExists(env_example)) moveSync(env_example, path_env);
+  
+  /* copying static files */
+  readdirSync(static_files).map(function(filename){
+    copySync(join(static_files, filename), join(dest, `/.${parse(filename).name}`));
+  });
+  console.log(`express app created at ${ dest }`);
 };
 
 main();
